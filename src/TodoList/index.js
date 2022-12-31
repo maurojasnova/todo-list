2fccfefb4bed7e658416c5f7154c4790a9464842
maurojasnova <andrsnov@gmail.com>
2022-12-30 22:46:17 -0500

    removed todos while the page is updating the data

--- src/TodoList/index.js
+++ src/TodoList/index.js
@@ -1,23 +1,20 @@
 import React from "react";
-import './TodoList.css'
+import "./TodoList.css";
 
 function TodoList(props) {
-    return (
-        <section className="TodoList-container">
-            {props.error && props.onError()}
-            {props.loading && props.onLoading()}
-            {(!props.loading && !props.totalTodos) && props.onEmptyTodos() }
-            {(!!props.totalTodos && !props.searchedTodos.length) && props.onEmptySearchResults(props.searchText)}
-            {props.searchedTodos.map(props.render)}
+  return (
+    <section className="TodoList-container">
+      {props.error && props.onError()}
+      {props.loading && props.onLoading()}
+      {!props.loading && !props.totalTodos && props.onEmptyTodos()}
+      {!!props.totalTodos &&
+        !props.searchedTodos.length &&
+        props.onEmptySearchResults(props.searchText)}
+      {!props.loading && !props.error && props.searchedTodos.map(props.render)}
 
-            <ul>
-                {props.children}
-            </ul>
+      <ul>{props.children}</ul>
     </section>
-    )
-
-
+  );
 }
 
-export {TodoList}
-
+export { TodoList };
